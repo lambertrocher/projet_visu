f189861a51a8cc8133fcd5a76124e3e9d208e882
Francois Gauthier <francois.gauthier@ensimag.grenoble-inp.fr>
2018-11-26 15:44:44 +0100

    supply line completed

--- Main.sh
+++ Main.sh
@@ -1,12 +1,15 @@
 #$1 est eccart à l'heure courante
 
 # Importation des données
-python ScriptRequeteMeteoFrance/RequeteAromeHD.py $1 SP1 #> data.grib2
+rm data.grib2
+python ScriptRequeteMeteoFrance/RequeteAromeHD.py $1 SP1
+mv *.grib2 data.grib2
+chmod +x wgrib2
 ./wgrib2
 ./wgrib2 data.grib2 -netcdf data.nc
 
 # Traitement des données
+pvpython ContourLine.py data.nc 250.0
 
 # Création des fichiers KML
 #Actuellement rien à faire (double clic sur test.kml)
-#A faire pour animation
