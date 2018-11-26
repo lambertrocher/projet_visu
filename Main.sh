ce87a33694d53b2b191ac2b05ada9b2d7db8d9e8
Francois Gauthier <francois.gauthier@ensimag.grenoble-inp.fr>
2018-11-26 14:36:07 +0100

    add chemin pour script

--- Main.sh
+++ Main.sh
@@ -1,7 +1,7 @@
 #$1 est eccart à l'heure courante
 
 # Importation des données
-python RequeteAromeHD.py $1 SP1 #> data.grib2
+python ScriptRequeteMeteoFrance/RequeteAromeHD.py $1 SP1 #> data.grib2
 ./wgrib2
 ./wgrib2 data.grib2 -netcdf data.nc
 
