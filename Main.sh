eb132112955b1fc7ca24089f3ca0558898a86b35
Maxime Jolivel <maxime.jolivel@grenoble-inp.org>
2018-11-26 16:56:49 +0100

    duplication kml

--- Main.sh
+++ Main.sh
@@ -9,7 +9,8 @@ chmod +x wgrib2
 ./wgrib2 data.grib2 -netcdf data.nc
 
 # Traitement des données
-pvpython ContourLine.py data.nc 250.0
+pvpython ContourLine.py data.nc $2 IsoContour.png
+pvpython StreamLine.py data.nc StreamLine.png
 
 # Création des fichiers KML
 #Actuellement rien à faire (double clic sur test.kml)
